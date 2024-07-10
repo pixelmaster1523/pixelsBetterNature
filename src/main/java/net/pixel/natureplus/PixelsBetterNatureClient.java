5998cddbcda90188fcaa0a9b2b52b2ee68260a3e
pixelmaster1523 <trwhitta@gmail.com>
2024-07-10 17:53:15 +1200

    Initial commit

---
+++ src/main/java/net/pixel/natureplus/PixelsBetterNatureClient.java
@@ -0,0 +1,10 @@
+package net.pixel.natureplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PixelsBetterNatureClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/pixel/natureplus/PixelsBetterNatureDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pixel.natureplus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PixelsBetterNatureDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
