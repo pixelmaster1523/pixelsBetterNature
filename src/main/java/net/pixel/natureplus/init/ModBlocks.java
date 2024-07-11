5171f6d4d53b3c9f3a5d151026a8073bbdbadb53
pixelmaster1523 <trwhitta@gmail.com>
2024-07-11 12:48:19 +1200

    Made a funny block with no texture but you get it on github because im lazy and i wanna use github more

---
+++ src/main/java/net/pixel/natureplus/init/ModBlocks.java
@@ -0,0 +1,15 @@
+package net.pixel.natureplus.init;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.pixel.natureplus.PixelsBetterNature;
+
+public class ModBlocks {
+    public static final Block EXAMPLE_BLOCK = register("example_block", new Block(AbstractBlock.Settings.create().strength(1.5F, 6.0F).requiresTool().luminance(state -> 15)));
+
+    public static <T extends Block> T register(String name, T block) {
+        return Registry.register(Registries.BLOCK, PixelsBetterNature.id(name), block);
+    }
+}
--- src/main/java/net/pixel/natureplus/init/ModItems.java
+++ src/main/java/net/pixel/natureplus/init/ModItems.java
@@ -1,5 +1,6 @@
 package net.pixel.natureplus.init;
 
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -7,11 +8,16 @@ import net.pixel.natureplus.PixelsBetterNature;
 
 public class ModItems {
 
-    public static final Item FUNNY_STICK = register("funny_stick", new Item(new Item.Settings()));
-
+//    Couldn't tell you what this does, just wrote it cos tutorial told me to.
     public static <T extends Item> T register(String name, T item) {
         return Registry.register(Registries.ITEM, PixelsBetterNature.id(name), item);
     }
+
+//    Item Registration
+    public static final Item FUNNY_STICK = register("funny_stick", new Item(new Item.Settings()));
+
+//    Block Item Registration
+    public static final BlockItem EXAMPLE_BLOCK = register("example_block", new BlockItem(ModBlocks.EXAMPLE_BLOCK, new Item.Settings()));
 
     public static void init() {}
 }
