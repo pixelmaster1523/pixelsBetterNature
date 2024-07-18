ef03816f9825196a7018529e2c24072a762df97b
pixelmaster1523 <trwhitta@gmail.com>
2024-07-18 15:32:43 +1200

    Testing blocks

--- src/main/java/net/pixel/natureplus/init/ModBlocks.java
+++ src/main/java/net/pixel/natureplus/init/ModBlocks.java
@@ -7,9 +7,11 @@ import net.minecraft.registry.Registry;
 import net.pixel.natureplus.PixelsBetterNature;
 
 public class ModBlocks {
-    public static final Block EXAMPLE_BLOCK = register("example_block", new Block(AbstractBlock.Settings.create().strength(1.5F, 6.0F).requiresTool().luminance(state -> 15)));
 
     public static <T extends Block> T register(String name, T block) {
         return Registry.register(Registries.BLOCK, PixelsBetterNature.id(name), block);
     }
+
+//    register blocks
+    public static final Block EXAMPLE_BLOCK = register("example_block", new Block(AbstractBlock.Settings.create().strength(1.5F, 6.0F).requiresTool().luminance(state -> 15)));
 }
